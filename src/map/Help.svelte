fb98691f9011c761e220d734f6e77af5ce362385
Zeden19 <ashar562@uwo.ca>
2023-06-08 22:48:38 -0400

    Add Help screen

--- src/map/Help.svelte
+++ src/map/Help.svelte
@@ -4,8 +4,27 @@
 
 
 <div>
-    <button>?</button>
+    <button onclick="help_dialog.showModal()">?</button>
 </div>
+
+<dialog style="width: 30%" id="help_dialog">
+    <p>Hi there, thanks for visiting! <br><br>
+
+        This website is a tool that allows individuals to find in-person Melee and Ultimate tournaments easier. <br><br>
+
+        To start, the search button on the right allows you to set filters and then search; the tournaments will appear
+        on the map below as markers that look like this:
+        <img style="vertical-align: middle;" src="https://maps.google.com/mapfiles/ms/icons/red-dot.png"
+             alt="red-pointer"><br><br>
+
+        Clicking on a marker will bring up a window with more information about the tournament. <br><br>
+
+        Clicking on the "legend" button on the right will show what the colors of the markers mean. <br><br>
+
+        I hope you enjoy using this website! <br><br>
+    </p>
+    <button onclick="help_dialog.close()">Close</button>
+</dialog>
 
 
 <style>
@@ -29,6 +48,14 @@
         position: absolute;
         right: 1%;
         top: 8%;
+    }
+
+    dialog {
+        border-radius: 10px;
+    }
+
+    dialog::backdrop {
+        background-color: rgba(0, 0, 0, 0.5);
     }
 
 </style>
