3553b2f6e03be65d4fc426e656e9ae20c4a55ca9
Zeden19 <ashar562@uwo.ca>
2023-06-12 21:52:05 -0400

    Fix outlook icon not appearing

--- src/contact/MailTo.svelte
+++ src/contact/MailTo.svelte
@@ -3,29 +3,39 @@
 </script>
 
 
-<a target="_blank" href="https://twitter.com/intent/follow?original_referer=https%3A%2F%2Fpublish.twitter.com%2F&ref_src=twsrc%5Etfw%7Ctwcamp%5Ebuttonembed%7Ctwterm%5Efollow%7Ctwgr%5EZedenZeder&screen_name=ZedenZeder">
-    Mail
-</a>
+<div>
+    <img src="outlook.png">
+    <a target="_blank" href="mailto:ashar562@uwo.ca">Mail</a>
+</div>
 
 
 <style>
     a {
-        text-align: right;
-        color: white;
-        background-size: 1.5em;
         font-weight: bold;
         text-decoration: none;
-        background: #005ea2 url("outlook.svg") no-repeat 0 50%;
+        color: white;
+        margin-right: 0;
+    }
+
+    div {
+        background-size: 1.5em;
+        background: #005ea2;
         border-radius: 30px;
-        display: inline-block;
         padding: 0 15px;
         width: 75px;
         height: 35px;
         line-height: 2;
+        display: flex;
+        justify-content: right;
     }
 
-    a:hover {
+    div:hover {
         background-color: #002844FF;
+    }
+    img {
+        justify-content: left;
+        margin-right: 19px;
+
     }
 
 </style>
