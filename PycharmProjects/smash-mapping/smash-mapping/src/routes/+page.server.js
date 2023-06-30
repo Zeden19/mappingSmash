6324771d68a54e5cf3a086fc73db46b829c1b117
Zeden19 <ashar562@uwo.ca>
2023-06-30 19:19:47 -0400

    Massive update lmao

---
+++ PycharmProjects/smash-mapping/smash-mapping/src/routes/+page.server.js
@@ -0,0 +1,38 @@
+import {GOOGLE_MAPS_API_KEY} from "$env/static/private";
+import {SMASH_GG_API_KEY} from "$env/static/private";
+import {SUPABASE_KEY} from "$env/static/private";
+import {supabase} from "$lib/supabaseClient";
+
+
+export async function load({cookies}) {
+    const {data} = await supabase.from("tournaments").select();
+    // const {data} = await supabase.channel('custom-insert-channel')
+    //     .on(
+    //         'postgres_changes',
+    //         {event: 'INSERT', schema: 'public', table: 'tournaments'},
+    //         (payload) => {
+    //             console.log('Change received!', payload)
+    //         }
+    //     ).subscribe()
+
+    // cookie stuff
+    let geolocated = cookies.get('geolocated');
+    const visited = cookies.get('visited');
+
+    if (geolocated === undefined) {
+        cookies.set('geolocated', 0, {path: '/'});
+    }
+
+    if (visited === undefined) {
+        cookies.set('visited', 0, {path: '/'});
+    }
+
+    return {
+        geolocated,
+        visited,
+        GOOGLE_MAPS_API_KEY,
+        SMASH_GG_API_KEY,
+        SUPABASE_KEY,
+        tournaments: data,
+    };
+}
